fa2e441a0a02b519ead97f35c9053e603040dea3
yash shukla <yashshuklas6@gmail.com>
2026-02-05 11:54:34 +0530

    s

--- my-app/src/socket.js
+++ my-app/src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const SOCKET_URL = "https://new-bvuzpz8aw-yashshukla011s-projects.vercel.app/"; 
+const SOCKET_URL = "https://new-bice-one-83.vercel.app/"; 
 
 export const socket = io(SOCKET_URL, {
     transports: ['websocket'], 
