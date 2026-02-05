e7902de3fec0ace8a10d713e3ef2fb6fa1fd7ae6
yash shukla <yashshuklas6@gmail.com>
2026-02-05 19:25:10 +0530

    s

--- my-app/src/socket.js
+++ my-app/src/socket.js
@@ -1,11 +1,12 @@
 import { io } from "socket.io-client";
 
-// IMPORTANT: Yahan apna real backend link hi dalna
-const SOCKET_URL = "https://new-production-132c.up.railway.app";
+// Purana link hata kar naya wala dalein
+const SOCKET_URL = "https://new-production-132c.up.railway.app"; 
+
 const socket = io(SOCKET_URL, {
   transports: ['websocket', 'polling'], 
   withCredentials: true,
-  autoConnect: false // Iska matlab hai socket tabhi start hoga jab hum bolenge
+  autoConnect: false 
 });
 
 export default socket;
