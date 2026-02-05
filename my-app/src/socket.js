b027d32f4f4a73576fdc638c29aa0698ab7fba32
yash shukla <yashshuklas6@gmail.com>
2026-02-05 12:08:53 +0530

    s

--- my-app/src/socket.js
+++ my-app/src/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 // Local testing ke liye aapka backend port 3001 hai
-const SOCKET_URL = "http://localhost:3001"; 
+const SOCKET_URL = "http://localhost:5173/"; 
 
 export const socket = io(SOCKET_URL, {
     transports: ['websocket', 'polling'], // Polling ko backup ke liye rakhein
